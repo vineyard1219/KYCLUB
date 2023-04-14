82c68aebb97a490274a4289222cebb187c2ab46b
김나경 <recodepan1219@egmail.com>
2023-04-14 17:22:17 +0900

    첨부 파일 있을 경우 포스트 상세 페이지에 다운로드 버튼 출력

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os #다운로드 파일 이름 알려줌
 
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목(글,길이30)
@@ -13,6 +14,11 @@ class Post(models.Model):
     def __str__(self):
         return f'[{self.pk}]{self.title}' #pk=번호, 제목
 
-
     def get_absolute_url(self):
         return f'/blog/{self.pk}/' #글 상페 url 설정
+        
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name) #다운로드 파일 이름 나타냄
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1] #다운로드 확장자 나타냄
