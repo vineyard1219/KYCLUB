caff665cfb2935305b67c30bb9bb0a3695032615
김나경 <recodepan1219@egmail.com>
2023-04-15 04:01:10 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User #유저 추가
 import os #다운로드 파일 이름 알려줌
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'	
 
     class Meta:
         verbose_name_plural = 'Categories'  #복수형
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목(글,길이30)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
 
@@ -12,6 +13,8 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
             
         return context
+    
+
 
 
 class PostDetail(DetailView):
@@ -23,6 +26,26 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         
         return context
+    
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        
+        }
+    )
     
         
 # Create your views here.
