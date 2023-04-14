0dec4febe74dcb06d432034d9ad2c0d3968f44dd
김나경 <recodepan1219@egmail.com>
2023-04-14 16:51:54 +0900

    포스트 목록, 상세 페이지에서 대표 이미지 없을 때 문제 해결하기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     content = models.TextField() #내용(글,무제한)
     
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #블로그 밑에 이미지/연월일까지 내려간 위치에서 저장/필수항목 아님
+    file_upload= models.FileField(upload_to='blog/images/%Y/%m/%d/', blank=True) #업로드 필드
+
     created_at = models.DateTimeField(auto_now_add=True) #작성일시(자동수정)
     updated_at = models.DateTimeField(auto_now=True) #시간 자동 업뎃
     
