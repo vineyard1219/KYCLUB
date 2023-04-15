74939d25eee330fc5e3760c12d9608a8559290d8
김나경 <recodepan1219@egmail.com>
2023-04-16 04:06:26 +0900

    검색 기능구현

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin #로그인할때만 정상 페이지
@@ -119,3 +120,20 @@ def delete_comment(request, pk):
     else:
         raise PermissionDenied
 # Create your views here.
+
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
