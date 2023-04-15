337484b9b776ac5f34416f52c85d1c4fbd492bfd
김나경 <recodepan1219@egmail.com>
2023-04-15 22:10:10 +0900

    포스트 수정 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin #로그인할때만 정상 페이지
 from .models import Post, Category
+from django.core.exceptions import PermissionDenied #글작성자만 수정가능
 
 
 class PostList(ListView):
@@ -15,8 +17,6 @@ class PostList(ListView):
         return context
     
 
-
-
 class PostDetail(DetailView):
     model = Post
     
@@ -26,8 +26,38 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         
         return context
+
+
+class PostCreate(LoginRequiredMixin, CreateView): #글작성 폼
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user #웹사이트 방문자
+        if current_user.is_authenticated: #로그인 유무 확인
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+                return redirect('/blog/')
+
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView): #글수정
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
     
-def category_page(request, slug):
+    template_name = 'blog/post_update_form.html'
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+    
+
+
+
+def category_page(request, slug): #카테고리
     if slug == 'no_category':
         category = '미분류'
         post_list = Post.objects.filter(category=None)
